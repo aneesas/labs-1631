b686fa2efe86c7d78f0d0a2c4d64163287ac09f6
Aneesa Sonawalla <aneesasona@gmail.com>
2023-10-10 19:53:50 -0400

    Task 1 scripts for saving and plotting sensor data

---
+++ scripts/load-sensor-data.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    data = np.load(sensor_data)
+
+    acceleration = data["acceleration"]  # 3x1, m/s^2
+    velocity = data["velocity"]  # 3x1, m/s
+    attitude = data["attitude"]  # 3x1, deg
+    height_sonar = data["h"]  # 1x1, m
+    height_baro = data["baro"]  # 1x1, m
+
+    plt.plot(acceleration)
+    plt.xlabel("Time (s)")  # TODO actually I think this will be sample #, not time
+    plt.ylabel("Acceleration (m/s^2)")
+    plt.legend()
+    plt.savefig("data-acceleration.png")
+    plt.close()
+
+    plt.plot(velocity)
+    plt.ylabel("Velocity (m/s)")
+    plt.legend()
+    plt.savefig("data-velocity.png")
+    plt.close()
+
+    plt.plot(attitude)
+    plt.ylabel("Attitude (deg)")
+    plt.legend()
+    plt.savefig("data-attitude.png")
+    plt.close()
+
+    plt.plot(height_sonar)
+    plt.ylabel("Height - Sonar (m)")
+    plt.legend()
+    plt.savefig("data-sonar.png")
+    plt.close()
+
+    plt.plot(height_baro)
+    plt.ylabel("Height - Baro (m)")
+    plt.legend()
+    plt.savefig("data-baro.png")
+    plt.close()
