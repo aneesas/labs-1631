ed69162061bc716f43c083326dc36d4bedc7985a
Aneesa Sonawalla <aneesasona@gmail.com>
2023-10-11 19:17:37 -0400

    Add missing time axis labels and fix layout of baro plot

--- scripts/load-sensor-data.py
+++ scripts/load-sensor-data.py
@@ -13,32 +13,35 @@ if __name__ == "__main__":
     time = np.arange(0, 5, 0.1) # sampled at 10 Hz
 
     plt.plot(time, acceleration, label=["x", "y", "z"])
-    plt.xlabel("Time (s)")  
+    plt.xlabel("Time (s)")
     plt.ylabel("Acceleration (m/s^2)")
     plt.legend()
     plt.savefig("data-acceleration.png")
     plt.close()
 
     plt.plot(time, velocity, label=["x", "y", "z"])
-    plt.xlabel("Time (s)")  
+    plt.xlabel("Time (s)")
     plt.ylabel("Velocity (m/s)")
     plt.legend()
     plt.savefig("data-velocity.png")
     plt.close()
 
     plt.plot(time, attitude, label=["x", "y", "z"])
-    plt.xlabel("Time (s)")  
+    plt.xlabel("Time (s)")
     plt.ylabel("Attitude (deg)")
     plt.legend()
     plt.savefig("data-attitude.png")
     plt.close()
 
     plt.plot(time, height_sonar)
+    plt.xlabel("Time (s)")
     plt.ylabel("Height - Sonar (m)")
     plt.savefig("data-sonar.png")
     plt.close()
 
     plt.plot(time, height_baro)
+    plt.xlabel("Time (s)")
     plt.ylabel("Height - Baro (m)")
     plt.savefig("data-baro.png")
+    plt.tight_layout()
     plt.close()
