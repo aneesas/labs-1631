9c9cb3fe492f8a378e9f63a3b8c11534ccb82140
Aneesa Sonawalla <aneesasona@gmail.com>
2023-10-11 19:14:38 -0400

    Debugging with drone

--- scripts/load-sensor-data.py
+++ scripts/load-sensor-data.py
@@ -2,42 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    data = np.load(sensor_data)
+    data = np.load("sensor_data.npy", allow_pickle=True).item()
 
     acceleration = data["acceleration"]  # 3x1, m/s^2
     velocity = data["velocity"]  # 3x1, m/s
     attitude = data["attitude"]  # 3x1, deg
-    height_sonar = data["h"]  # 1x1, m
+    height_sonar = data["height"]  # 1x1, m
     height_baro = data["baro"]  # 1x1, m
 
-    # TODO might have to do accel/vel/attitude as 3 separate plot calls
-    plt.plot(acceleration)
-    plt.xlabel("Time (s)")  # TODO actually I think this will be sample #, not time
+    time = np.arange(0, 5, 0.1) # sampled at 10 Hz
+
+    plt.plot(time, acceleration, label=["x", "y", "z"])
+    plt.xlabel("Time (s)")  
     plt.ylabel("Acceleration (m/s^2)")
     plt.legend()
     plt.savefig("data-acceleration.png")
     plt.close()
 
-    plt.plot(velocity)
+    plt.plot(time, velocity, label=["x", "y", "z"])
+    plt.xlabel("Time (s)")  
     plt.ylabel("Velocity (m/s)")
     plt.legend()
     plt.savefig("data-velocity.png")
     plt.close()
 
-    plt.plot(attitude)
+    plt.plot(time, attitude, label=["x", "y", "z"])
+    plt.xlabel("Time (s)")  
     plt.ylabel("Attitude (deg)")
     plt.legend()
     plt.savefig("data-attitude.png")
     plt.close()
 
-    plt.plot(height_sonar)
+    plt.plot(time, height_sonar)
     plt.ylabel("Height - Sonar (m)")
-    plt.legend()
     plt.savefig("data-sonar.png")
     plt.close()
 
-    plt.plot(height_baro)
+    plt.plot(time, height_baro)
     plt.ylabel("Height - Baro (m)")
-    plt.legend()
     plt.savefig("data-baro.png")
     plt.close()
