4d6d4343a83cb5732aae6e37ab86467fcfb17d3c
Aneesa Sonawalla <aneesasona@gmail.com>
2023-12-05 19:55:57 -0500

    Remove unused functions from utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -16,27 +16,6 @@ def fly_open_loop(pilot, xyz_velocity, total_time, command_rate):
     return
 
 
-def get_gate_center(pilot: tx.Pilot, tags: list, top_left_idx: int):
-    """
-    Takes list of Detection objects defining a gate and returns a numpy array denoting the
-    center of the gate in the frame of the top-left tag at the given index
-    """
-    poses_camera_frame = np.zeros((len(tags), 3))
-    for idx, t in enumerate(tags):
-        poses_camera_frame[idx, :] = t.pose_t.reshape(3,)
-    gate_center_camera_frame = np.mean(poses_camera_frame, axis=0)
-    # Camera frame is +x right, +y down, +z forward, so the center will be positive distance from
-    # the top-left tag of the gate
-    distance_camera_frame = gate_center_camera_frame - poses_camera_frame[top_left_idx, :]
-    print("Computed distance in camera frame = gate_center - top_left_pose_camera_frame:")
-    print("\t", distance_camera_frame)
-    gate_center_y_dist = distance_camera_frame[0]
-    gate_center_z_dist = distance_camera_frame[1]
-    # drone_pose_global, _, _ = pilot.get_drone_pose(tags[top_left_idx])
-    # return np.array([drone_pose_global[0], drone_pose_global[1] + gate_center_y_dist, drone_pose_global[2] + gate_center_z_dist])
-    return (gate_center_y_dist, gate_center_z_dist)
-
-
 def get_pose_gate_center(pilot: tx.Pilot, tags: list):
     """
     Takes list of Detection objects defining a gate and returns a numpy array denoting the
@@ -49,17 +28,3 @@ def get_pose_gate_center(pilot: tx.Pilot, tags: list):
     return pose_gate_center
 
 
-def get_top_left_tag_idx(tags: list):
-    if len(tags) == 0:
-        print("ERROR: Empty list of tags given!")
-        return -1
-
-    top_left_idx = 0
-    leftmost_pos = tags[0].pose_t[0]  # +x to the right
-    topmost_pos = tags[0].pose_t[1]  # +z down
-    for idx, t in enumerate(tags):
-        if t.pose_t[0] < leftmost_pos or t.pose_t[1] < topmost_pos:
-            top_left_idx = idx
-            leftmost_pos = t.pose_t[0]
-            topmost_pos = t.pose_t[1]
-    return top_left_idx
