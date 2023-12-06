8a6eaf9e3fe47dca257f75bec96416f6103fe781
Aneesa Sonawalla <aneesasona@gmail.com>
2023-12-06 10:22:46 -0500

    Consolidate utils functions into one file

--- scripts/Final_Code.py
+++ scripts/Final_Code.py
@@ -5,8 +5,6 @@ import cv2
 
 import tellox as tx
 
-from utils import *
-
 # Constants/environment definition
 MAX_HEIGHT = 8  # meters; depends on room, used for safety checks
 MAX_VEL_MAG = 0.5  # m/s; for safety
@@ -31,6 +29,30 @@ L2 = 4
 X_THRESH = 0.1  # meters
 Y_THRESH = 0.15
 Z_THRESH = 0.5
+
+
+def fly_open_loop(pilot, xyz_velocity, total_time, command_rate):
+    time_elapsed = 0.0
+    dt = 1.0/command_rate
+    while time_elapsed < total_time:
+        pilot.send_control(xyz_velocity, 0.0)
+        time.sleep(dt)
+        time_elapsed += dt
+    pilot.send_control(np.array([0.0, 0.0, 0.0]), 0.0)
+    print("Completed open-loop flight for {} seconds at {} m/s.".format(time_elapsed, xyz_velocity))
+    return
+
+
+def get_pose_gate_center(pilot: tx.Pilot, tags: list):
+    """
+    Takes list of Detection objects defining a gate and returns a numpy array denoting the
+    center of the gate
+    """
+    drone_poses = np.zeros((len(tags), 3))
+    for idx, t in enumerate(tags):
+        drone_poses[idx, :], _, _ = pilot.get_drone_pose(t)
+    pose_gate_center = np.mean(drone_poses, axis=0)
+    return pose_gate_center
 
 
 if __name__ == "__main__":
